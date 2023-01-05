0ef92b32c868ff2538c8e3f070c44a04e03c075a
mariuskachel <m.kachel@strich-komma.de>
2023-01-05 15:10:59 +0100

    Update description

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'httpmonitoring',
-    'description' => 'An extension for the TYP3 CMS which adds a backend module for monitoring the HTTP status code given out by sites.',
+    'description' => 'An extension for the TYPO3 CMS which adds a backend module for monitoring the HTTP status code given out by sites.',
     'category' => 'module',
     'author' => 'Marius Kachel',
     'author_email' => 'marius.kachel@p2media.de',
     'author_company' => 'p2media',
     'state' => 'beta',
-    'version' => '1.0.2',
+    'version' => '1.0.3',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
